b3f2054cf4a507a7c06b676b5253cbe0c4ccab66
ansible <ansible@home>
2022-07-28 14:29:52 -0400

    usability tweaks

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,5 +1,6 @@
 #instructions from https://www.tecmint.com/install-docker-in-rocky-linux-and-almalinux/
 USER="ansible"
+echo "User set to $USER"
 
 #"On your terminal, run the following command to add the Docker repository"
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
@@ -12,11 +13,10 @@ docker --version
 #Start and Enable Docker
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo systemctl status docker
+sudo systemctl status docker |grep Active:
 
 #"To use or run docker as a regular user, you need to add the user to the ‘docker‘ group which is automatically created during installation. Otherwise, you will keep on running into permission errors."
 sudo usermod -aG docker "$USER"
-id $USER
 echo "You must log out and log back if you run into permissions errors"
 
 
