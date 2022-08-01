20b636e5026680b70433ef6b74b3d096d6af93c0
ansible <ansible@home>
2022-08-01 18:28:18 -0400

    sudo to ansible and moved a docker-compose.yml to scripts folder

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -4,20 +4,20 @@
 
 
 # "python3-argcomplete is provided by EPEL. Please install epel-release if not done yet. This package will help you complete Ansible commands."
-dnf install epel-release -y
+sudo dnf install epel-release -y
 
 # "As we want to use a newer version of Ansible, we will install it from python3-pip"
-dnf install python38 python38-pip python38-wheel python3-argcomplete rust cargo curl -y
+sudo dnf install python38 python38-pip python38-wheel python3-argcomplete rust cargo curl -y
 
 # "Before we actually install Ansible, we need to tell Rocky Linux that we want to use the newly installed version of Python. The reason is that if we continue to the install without this, the default python3 (version 3.6 as of this writing), will be used instead of the newly installed version 3.8. Set the version you want to use by entering the following command:"
 
 # In Rocky 8.5 it looks like only this first one is needed, buy we'll run both anyway
-alternatives --set python /usr/bin/python3.8
-alternatives --set python3 /usr/bin/python3.8
+sudo alternatives --set python /usr/bin/python3.8
+sudo alternatives --set python3 /usr/bin/python3.8
 
 # "We can now install Ansible:"
-pip3 install ansible
-activate-global-python-argcomplete
+sudo pip3 install ansible
+sudo activate-global-python-argcomplete
 
 # "An example of the ansible.cfg is given here and an example of the hosts file here.
 # $ sudo mkdir /etc/ansible
@@ -26,7 +26,7 @@ activate-global-python-argcomplete
 # You can also use the ansible-config command to generate a new configuration file:"
 #  No Worky-> ansible-config init --disabled > /etc/ansible/ansible.cfg
 # The command given at this point fails with a "no such file or directory lets add it.
-mkdir /etc/ansible
+sudo mkdir /etc/ansible
 
 # The command given still fails but now as command not found. Remember we're running as root.
 # which ansible-config shows:
@@ -36,7 +36,7 @@ mkdir /etc/ansible
 # An answer to why can be found here: https://unix.stackexchange.com/questions/115129/why-does-root-not-have-usr-local-in-path from 8 years ago (as of 2022)
 # My guess is this is because we didn't install from the epel-release repo, we used pip3 instead
 # We could add the /usr/local/bin to the root path, or leave it as is and move on.
-/usr/local/bin/ansible-config init --disabled > /etc/ansible/ansible.cfg
+sudo /usr/local/bin/ansible-config init --disabled > /etc/ansible/ansible.cfg
 
 # We did it!
-/usr/local/bin/ansible --version
+sudo /usr/local/bin/ansible --version
