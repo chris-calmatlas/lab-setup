e3a89ed36502610b9085d12a7d72eda7c7b027ba
chris j <chris@calmatlas.com>
2022-09-22 22:52:56 +0000

    corrects to variables

--- scripts/install_wordpress_debian.sh
+++ scripts/install_wordpress_debian.sh
@@ -4,11 +4,12 @@
 #Debian 11 bullseye (Google Cloud COmpute e2 medium instance)
 
 #variables
-SITENAME="blog1"
-SERVER_FQDN="$SITENAME.calmatlas.com"
+SITENAME="blog5"
+FQDN="$SITENAME.calmatlas.com"
 #email address needed for letsencrypt, if you comment the last part of this
 #script, then you can also leave this variable blank
 EMAIL=chris@calmatlas.com
+WP_USER=wp_$SITENAME
 
 #First update packages
 sudo apt update -y && sudo apt upgrade -y
@@ -28,20 +29,20 @@ sudo apt install mariadb-server -y
 sudo systemctl enable --now mariadb
 
 #Remove the mysql root password first if it exists
-sudo mysql --defaults-file=/root/wp_root.pass --execute --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY ''"
+sudo mysql --defaults-file=/root/wp_root.pass --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY ''"
 
 #Create a databsae
-sudo mysql --user="root" --execute "CREATE DATABASE $wp_$SITENAME"
+sudo mysql --user="root" --execute "CREATE DATABASE wp_$SITENAME"
 
 #We're going to use a random password for the wordpress user for this script
 wp_db_user_pass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 
 #Create the user
-sudo mysql --user="root" --database="$wp_$SITENAME" --execute="CREATE USER 'wp_user'@'localhost' IDENTIFIED BY '$wp_db_user_pass'"
+sudo mysql --user="root" --database="wp_$SITENAME" --execute="CREATE USER '$WP_USER'@'localhost' IDENTIFIED BY '$wp_db_user_pass'"
 
 #Grant the user privileges
-sudo mysql --user="root" --database="$wp_$SITENAME" --execute="GRANT ALL ON wordpressdb.* TO 'wp_user'@'localhost'"
-sudo mysql --user="root" --database="$wp_$SITENAME" --execute="FLUSH PRIVILEGES"
+sudo mysql --user="root" --database="wp_$SITENAME" --execute="GRANT ALL ON wordpressdb.* TO '$WP_USER'@'localhost'"
+sudo mysql --user="root" --database="wp_$SITENAME" --execute="FLUSH PRIVILEGES"
 
 #We should improve the security of the mariadb installation, the following command promps the user
 #sudo mysql_secure_installation
@@ -64,19 +65,20 @@ sudo mysql --user="root" --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 
 sudo mysql --user="root" --password="$wp_db_root_pass" --execute="FLUSH PRIVILEGES"
 
 #Download the latest worpdress
-curl https://wordpress.org/latest.tar.gz --output wordpress.tar.gz
+curl https://wordpress.org/latest.tar.gz --output ~/wordpress.tar.gz
 
 #Extract the wordpress files to apache directory
-sudo tar -xf wordpress.tar.gz -C /var/www/html
+sudo tar -xf ~/wordpress.tar.gz -C /var/www/html
+sudo mv /var/www/html/wordpress /var/www/html/$SITENAME
 
 #give apapche ownership to wordpress
-sudo chown -R www-data:www-data /var/www/html/$FQDN
+sudo chown -R www-data:www-data /var/www/html/$SITENAME
 
 #set permissions to wordpress
-sudo chmod -R 775 /var/www/html/$FQDN
+sudo chmod -R 775 /var/www/html/$SITENAME
 
 #Create an apache virtual host file to point to the wordpress install
-cat > ./wordpress.conf << EOF
+cat > ./$SITENAME.conf << EOF
 <VirtualHost *:80>
   ServerName $FQDN
   Redirect permanent / https://$FQDN/
@@ -86,11 +88,11 @@ cat > ./wordpress.conf << EOF
   ServerName $FQDN
 
   ServerAdmin root@localhost
-  DocumentRoot /var/www/html/$FQDN
+  DocumentRoot /var/www/html/$SITENAME
   ErrorLog /var/log/apache2/wordpress_error.log
   CustomLog /var/log/apache2/wordpress_access.log common
 
-  <Directory "/var/www/html/$FQDN">
+  <Directory "/var/www/html/$SITENAME">
     Options Indexes FollowSymLinks
     AllowOverride all
     Require all granted
@@ -100,9 +102,9 @@ cat > ./wordpress.conf << EOF
 EOF
 
 #Move the file we just created and give it the appropriate permissions
-sudo chown root:root ./$FQDN.conf
-sudo mv ./FQDN.conf /etc/apache2/sites-available
-sudo a2ensite $FQDN
+sudo chown root:root ./$SITENAME.conf
+sudo mv ./$SITENAME.conf /etc/apache2/sites-available
+sudo a2ensite $SITENAME
 
 #we have a working webserver at this point point but can't login to wordpress admin because of cert errors
 #Let's use letsencrypt to get some valid certs.
@@ -116,7 +118,7 @@ sudo certbot --apache --non-interactive --agree-tos -m $EMAIL --domain $FQDN
 #give username and password
 echo ""
 echo "Navigate to your site in a browser and use the following information"
-echo "Wordpress User: wp_user"
+echo "Wordpress User: $WP_USER"
 echo "Password: $wp_db_user_pass"
 echo ""
 echo "Copy this info. You won't see it again."
